d1feb644a26028549d50c833542658dc97fa3f69
hossein-heydari <hosseinheydari790228@gmail.com>
2021-06-08 00:00:45 +0430

    done

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 let array = [];
 
